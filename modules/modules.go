af86b681659e72a4ecfa91317cc95fce0d4cbaa6
Fernando Ayats <ayatsfer@gmail.com>
2025-07-03 22:34:42 +0200

    Tweak module

--- modules/modules.go
+++ modules/modules.go
@@ -1,13 +1,37 @@
 package modules
 
-import "os"
+import (
+	"os"
+	"strings"
 
-type Directory struct {}
+	"github.com/viperML/batonik/colors"
+)
+
+type Directory struct {
+	Style func(string) string
+}
+
+func defaultStyle(text string) string {
+	return colors.Blue + text + colors.Reset
+}
 
 func (d *Directory) Run() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "Error getting home directory"
+	}
+
 	cwd, err := os.Getwd()
 	if err != nil {
 		return "Error getting directory"
 	}
-	return cwd
+
+	// Replace home directory with ~
+	cwd = strings.Replace(cwd, home, "~", 1)
+
+	if d.Style == nil {
+		d.Style = defaultStyle
+	}
+
+	return d.Style(cwd)
 }
