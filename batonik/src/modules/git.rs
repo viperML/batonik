1ba2e3028d80e628dbb36e9f2cce57a0def14f46
Fernando Ayats <ayatsfer@gmail.com>
2025-07-14 14:58:30 +0200

    Add gix

--- batonik/src/modules/git.rs
+++ batonik/src/modules/git.rs
@@ -1,9 +1,22 @@
+use tokio::task::spawn_blocking;
+
 use crate::Module;
 
 pub struct Git;
 
 impl Module for Git {
     async fn run(self) -> String {
-        return String::from("branch: master");
+        let hn = spawn_blocking(|| {
+            let repo = match gix::discover(".") {
+                Ok(repo) => repo,
+                Err(_) => return String::default(),
+            };
+
+            repo.head_name().ok().flatten().unwrap().to_string()
+        })
+        .await
+        .unwrap();
+
+        format!("branch: {hn}")
     }
 }
